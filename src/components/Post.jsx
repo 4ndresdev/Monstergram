5a0a4d466f37641aa267cd2f95fafe3bb5b204c9
4ndresdev <4ndresdev@gmail.com>
2024-10-18 20:50:24 -0600

    create profile

---
+++ src/components/Post.jsx
@@ -0,0 +1,10 @@
+const Post = () => {
+  return (
+    <div>
+      <h1 className="text-2xl font-semibold text-center">Post</h1>
+      <p className="text-center text-gray-400">This is a post</p>
+    </div>
+  );
+};
+
+export default Post;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,21 @@
 import Header from "../components/Header";
+import UserProfile from "../components/UserProfile";
+import Post from "../components/Post";
 
 const Home = () => {
   return (
     <>
       <Header />
-      <div className="container grid gap-4 grid-cols-12 mt-5">
-        <div className="hidden md:flex col-span-3 bg-slate-400">sdf</div>
-        <div className="col-span-12 md:col-span-6 bg-slate-400">sdf</div>
-        <div className="hidden md:flex md:col-span-3 bg-slate-400">sdfsd</div>
+      <div className="container grid gap-4 grid-cols-12">
+        <div className="hidden lg:flex col-span-3 rounded-t-lg max-h-80 sticky top-0">
+          <UserProfile />
+        </div>
+        <div className="col-span-12 lg:col-span-6 bg-slate-400 mt-5">
+          <Post />
+        </div>
+        <div className="hidden lg:flex lg:col-span-3 bg-slate-400 max-h-80 mt-5">
+          sdfsd
+        </div>
       </div>
     </>
   );
